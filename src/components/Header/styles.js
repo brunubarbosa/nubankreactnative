8387e7b15452232cbb35a130094cad44c0ce0608
brunubarbosa <brunopjsw@gmail.com>
2020-02-23 23:30:06 -0300

    first commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #8B10AE;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const Logo = styled.Image`
+  width: 20;
+  height: 20;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  margin-left: 8px;
+`;
---
+++ src/components/Tabs/styles.js
@@ -0,0 +1,21 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.View`
+  align-items: center;
+  padding: 40px 0 30px;
+`;
+
+export const Top = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+export const Logo = styled.Image`
+
+`;
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #FFF;
+  font-weight: bold;
+  margin-left: 8px;
+`;
